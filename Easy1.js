4aca7f26b09af4bdf46bf80014c4d7bce6d02d2e
Sergio <smunoz65@gmail.com>
2020-06-16 21:20:42 +0100

    Completed Product and Sum function

--- Easy1.js
+++ Easy1.js
@@ -14,12 +14,14 @@ let oddNumbers = (int) => [...Array(int + 1).keys()]
                            .filter(x => x % 2 !== 0)
                            .forEach(x => console.log(x));
 
-//Re-using the previous function.
+/*Re-using the previous function.
+Commented this one out to avoid having issues with redeclaring the same
+variable twice
+
 let oddNumbers = (int) => [...Array(int + 1).keys()]
                          .filter(isOdd)
                          .forEach(x => console.log(x));
 
-/*
 Log all even numbers from 1 to 99, inclusive, to the console. 
 Log all numbers on separate lines.
 */
@@ -42,3 +44,31 @@ let readlineSync = require('readline-sync');
 let length = readlineSync.question("Please enter the length of the room. ");
 let width = readlineSync.question("Please enter the width of the room. ");
 console.log(`The are of the room is ${length*width}: ${((length*width)*10.7639)}`);
+
+/*
+Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip, and then log both the tip and the total amount of the bill to the console. 
+You can ignore input validation and assume that the user will enter numbers.
+*/
+
+let bill = readlineSync.question("What is the bill amount?: ");
+let tipPercentage = readlineSync.question("What is the tip percentage?: ");
+let tip = bill * (tipPercentage/100);
+console.log(`The tip is ${tip}`);
+console.log(`The total is $${Number(bill)+Number(tip)}`);
+
+/*
+Write a program that asks the user to enter an integer greater than 0, then asks whether the user 
+wants to determine the sum or the product of all numbers between 1 and the entered integer, inclusive.
+*/
+
+let number = readlineSync.question("Please enter an integer greater than 0: ");
+let sumOrProd = readlineSync.question('Enter "s" to compute the sum, or "p" to compute the product. ');
+
+// We need to remove the first element which will be zero for the product function to work.
+let arraynumbers = [...Array(Number(number)+1).keys()].slice(1);
+
+let sumNumbers = (array) => array.reduce((acc,x)=> acc + x, 0);
+let multNumbers = (array) => array.reduce((acc,x)=> acc * x, 1);
+
+let userdecision = sumOrProd.toLowerCase() === 's' ? sumNumbers(arraynumbers) : multNumbers(arraynumbers);
+console.log(userdecision);
