b23bd0142e1a4b825aebc165cf3209b194ccc752
Sergio <smunoz65@gmail.com>
2020-06-17 19:29:56 +0100

    Started Easy3

---
+++ Easy3.js
@@ -0,0 +1,97 @@
+/*
+Write a function that takes a string, doubles every character in the string, 
+and returns the result as a new string.
+
+Examples:
+
+repeater('Hello');        // "HHeelllloo"
+repeater('Good job!');    // "GGoooodd  jjoobb!!"
+repeater('');
+*/
+
+let repeater = (word) => word.split("").map(x => x + x).join("");
+
+/*
+Write a function that takes a string, doubles every consonant character in the string, 
+and returns the result as a new string. The function should not double vowels 
+('a','e','i','o','u'), digits, punctuation, or whitespace.
+
+Examples:
+
+doubleConsonants('String');          // "SSttrrinngg"
+doubleConsonants('Hello-World!');    // "HHellllo-WWorrlldd!"
+doubleConsonants('July 4th');        // "JJullyy 4tthh"
+doubleConsonants(''); 
+*/
+
+let doubleConsonants = (word) => word.split("").map(x => /[^aeiou\s\W]/gi.test(x) ? x + x : x).join("");
+
+/*
+Write a function that takes a positive integer as an argument, and returns 
+that number with its digits reversed.
+
+Examples:
+
+reverseNumber(12345);    // 54321
+reverseNumber(12213);    // 31221
+reverseNumber(456);      // 654
+reverseNumber(12000);    // 21 -- Note that leading zeros in the result get dropped!
+reverseNumber(1);
+*/
+
+let reverseNumber = (number) => Number(String(number).split("").reverse().join(""));
+
+/*
+Write a function that takes a non-empty string argument, and returns the middle character(s) 
+of the string. If the string has an odd length, you should return exactly one character. 
+If the string has an even length, you should return exactly two characters.
+
+Examples:
+
+centerOf('I Love JavaScript'); // "a"
+centerOf('Launch School');     // " "
+centerOf('Launch');            // "un"
+centerOf('Launchschool');      // "hs"
+centerOf('x'); 
+*/
+
+let centerOf = (word) => word.length % 2 === 0 ? word[(word.length-1)/2 - 0.5] + word[(word.length-1)/2 +0.5] :
+                         word.length === 1 ? word : word[(word.length-1)/2];
+
+/*
+Write a function that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the argument is a negative number, return it as-is.
+
+Examples:
+
+negative(5);     // -5
+negative(-3);    // -3
+negative(0);     // -0
+*/
+
+let negative = (number) => number < 0 ? number : -number;
+
+/*
+Write a function that takes an integer argument, and returns an array containing 
+all integers between 1 and the argument (inclusive), in ascending order.
+
+You may assume that the argument will always be a positive integer.
+
+Examples:
+
+sequence(5);    // [1, 2, 3, 4, 5]
+sequence(3);    // [1, 2, 3]
+sequence(1); 
+*/
+
+let sequence = (number) => [...Array(number + 1).keys()].slice(1);
+
+/*
+Write a function that takes a string argument consisting of a first name, 
+a space, and a last name, and returns a new string consisting of the last name, a comma, a space, and the first name.
+
+Examples:
+
+swapName('Joe Roberts');    // "Roberts, Joe"
+*/
+
+let swapName =(name) => name.split(" ").reverse().join(" ");
