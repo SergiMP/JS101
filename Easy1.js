df6af51ce47b873687ea69581942ce7efd8ca832
SergiMP <smunoz02@dcs.bbk.ac.uk>
2020-06-17 16:24:52 +0100

    Completed Easy 1

--- Easy1.js
+++ Easy1.js
@@ -78,3 +78,52 @@ You may assume that the strings are of different lengths.
 */
 
 let shortLongShort = (a,b) =>  a.length < b.length ? a+b+a : b+a+b;
+
+/*
+In the modern era under the Gregorian Calendar, leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100. 
+If the year is evenly divisible by 100, then it is not a leap year, unless the year is also evenly divisible by 400.
+
+Assume this rule is valid for any year greater than year 0. Write a function that takes any year greater than 0 as input, 
+and returns true if the year is a leap year, or false if it is not a leap year.
+*/
+
+let isLeapYear = (year) =>{
+    return year % 4 === 0 && (!(year % 100 === 0) || (year % 400 ===0));
+}
+
+/*
+The British Empire adopted the Gregorian Calendar in 1752, which was a leap year. Prior to 1752, they used the Julian Calendar. Under the Julian Calendar, leap years occur in any year that is evenly divisible by 4.
+
+Using this information, update the function from the previous exercise to determine leap years both before and after 1752.
+*/
+
+let JulianGregorianLeapYear = (year) =>{
+    let gregorian = year % 4 === 0 && (!(year % 100 === 0) || (year % 400 ===0));
+    let julian = year % 4 === 0;
+    return year > 1752 ? gregorian : julian;
+}
+
+/*
+Write a function that computes the sum of all numbers between 1 and some other number, 
+inclusive, that are multiples of 3 or 5. For instance, if the supplied number is 20,
+ the result should be 98 (3 + 5 + 6 + 9 + 10 + 12 + 15 + 18 + 20).
+You may assume that the number passed in is an integer greater than 1.
+
+Examples:
+*/
+
+let multisum = (number) =>{
+    let numbers = [...Array(number +1).keys()];
+    return numbers.filter( x => x % 3 === 0 || x % 5 === 0).reduce((acc,x)=> acc + x,0);
+}
+
+/*
+Write a function that determines and returns the ASCII string value of a string passed in as an argument. 
+The ASCII string value is the sum of the ASCII values of every character in the string. 
+(You may use String.prototype.charCodeAt() to determine the ASCII value of a character.)
+asciiValue('Four score'); //// 984
+*/
+
+let asciiValue = (word) => word.split("").map(x => x.charCodeAt(0))
+                                         .reduce((acc,x)=> acc +x,0);
+
