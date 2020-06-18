b96df34dd8a30521da4a05ebc23b0320e2d3eaad
SergiMP <smunoz02@dcs.bbk.ac.uk>
2020-06-18 16:40:26 +0100

    Update Easy3.js

--- Easy3.js
+++ Easy3.js
@@ -78,3 +78,41 @@ stringy(7);    // "1010101"
 */
 
 let stringy = (int) => [...Array(int).keys()].map((elem,idx)=>idx %2 ==0? "1":"0").join("");
+
+/*
+The Fibonacci series is a series of numbers (1, 1, 2, 3, 5, 8, 13, 21, ...) such that the first two numbers are 1 by definition, and each subsequent number is the sum of the two previous numbers.
+This series appears throughout the natural world.
+
+Computationally, the Fibonacci series is a simple series, but the results grow at an incredibly rapid rate. 
+For example, the 100th Fibonacci number is 354,224,848,179,261,915,075—that's enormous, especially considering that it takes six iterations just to find the first 2-digit Fibonacci number.
+
+Write a function that calculates and returns the index of the first Fibonacci number that has the number of digits specified by the argument. 
+(The first Fibonacci number has an index of 1.)
+
+You may assume that the argument is always an integer greater than or equal to 2.
+
+Examples:
+
+findFibonacciIndexByLength(2);       // 7
+findFibonacciIndexByLength(10);      // 45
+findFibonacciIndexByLength(16);      // 74
+*/
+// Don't try any higher values until you read the solution Discussion
+
+// This solution doesn't use recursion or memoization just to follow the instructions.
+
+let findFibonacciIndexByLength = (num) =>{
+    let container = ["0","1","1"]
+    let idx = 0;
+    let first = 1;
+    let second = 1;
+    let fibonaccinumber = first + second;
+    while(container[idx].length !== num){
+        container.push(String(fibonaccinumber));
+        first = second;
+        second = fibonaccinumber;
+        fibonaccinumber = first + second;
+        idx += 1;
+    }
+    return idx;
+}
