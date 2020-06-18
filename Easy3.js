6abaae49e14f8ece4d93afdb0216d7bf29289ab0
SergiMP <smunoz02@dcs.bbk.ac.uk>
2020-06-18 16:09:47 +0100

    Update Easy3.js

--- Easy3.js
+++ Easy3.js
@@ -63,3 +63,18 @@ let logInBox = (string) =>{
     console.log(filler.join(""));
     console.log(topLine.join(""));
 }
+
+/*
+Write a function that takes one argument, a positive integer, and returns a string of alternating '1's and '0's, 
+always starting with a '1'. The length of the string should match the given integer.
+
+Examples:
+
+stringy(6);    // "101010"
+stringy(9);    // "101010101"
+stringy(4);    // "1010"
+stringy(7);    // "1010101"
+
+*/
+
+let stringy = (int) => [...Array(int).keys()].map((elem,idx)=>idx %2 ==0? "1":"0").join("");
