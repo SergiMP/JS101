4f2f6b3829da51d06767807641c10b95d79f3b34
Sergio <smunoz65@gmail.com>
2020-06-18 19:36:26 +0100

    Update Easy3.js

--- Easy3.js
+++ Easy3.js
@@ -188,3 +188,15 @@ let twice = (number) =>{
             return first === second ? number : number * 2;
         }
 }
+
+/*
+Given a string that consists of some words and an assortment of non-alphabetic characters, 
+write a function that returns that string with all of the non-alphabetic characters replaced by spaces. If one or more non-alphabetic characters occur in a row, you should only have one space in the result (i.e., the result string should never have consecutive spaces).
+
+Example:
+
+cleanUp("---what's my +*& line?");    // " what s my line "
+*/
+
+let cleanUp = (word) => word.split("").map(x =>/[a-z]/i.test(x) ? x : " ").join("")
+                                      .replace(/  +/g," ")
