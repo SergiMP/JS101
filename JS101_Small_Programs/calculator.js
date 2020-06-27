ed36a63ee2505ef06024913f7a757d2b812a26e2
Sergio <smunoz65@gmail.com>
2020-06-28 00:26:55 +0100

    Reworked calculator
    Added Json config file with languages and replaced the text by variables.

--- JS101_Small_Programs/calculator.js
+++ JS101_Small_Programs/calculator.js
@@ -1,12 +1,7 @@
-/*
-Greet the user.
-Ask the first number.
-Ask the second number.
-Perform the requested operation.
-Output the result
-*/
 const readSync = require("readline-sync");
+const messages = require("./config.json");
 
+//Declare and set output format function.
 const bannerizeInput = (text) => {
   let topLine = ["*"];
   for (let int = 0; int <= text.length + 1; int++ ) {
@@ -18,56 +13,75 @@ const bannerizeInput = (text) => {
   let textLine = `| ${text} |`;
   return ` ${topLine}\n ${fillerLine}\n ${textLine.replace(/\n/g,'')}\n ${fillerLine}\n ${topLine}`;
 };
-
+//Declare and set mathematical functions
 const add = (x,y) => x + y;
 const substract = (x,y) => x - y;
 const multiply = (x,y) => x * y;
 const divide = (x,y) => x / y;
-
+//Declare and set dictionary as commands : functions
 let operations = {
   "+" : add, "-" : substract,
   "*" : multiply, "/" : divide,
   "add" : add, "substract" : substract,
   "multiply" : multiply, "divide" : divide,
+  "sumar" : add, "restar" : substract,
+  "multipliclar" : multiply, "dividir" : divide,
   "1" : add, "2" : substract, "3" : multiply, "4" : divide
 };
+//Declare while loop conditions.
+let carryOn = true;
+let confirm;
+const acceptableAnswers = ['yes',"y","yas","no",'n','nope'];
 
-const greet = `Welcome to this calculator 
-               you will be asked to enter two numbers one at a time.`;
+//Greet user
+console.log(bannerizeInput(`Greetings, welcome to our calculator`));
+console.log(bannerizeInput(` Please type "es" to switch to the Spanish version of this calculator or "en" for the english version`));
+let language = readSync.question("Language (default: en) ===>  ").toLowerCase();
+let langVersion = language === "es".trim() ? messages["es"] : messages["en"];
 
-console.log(bannerizeInput(greet.replace(/\s{2}/g,"")));
+console.log(bannerizeInput(langVersion.greeting.replace(/\s{2}/g,"")));
+//Begin loop, ask numbers, operator, print results.
+while (carryOn !== false) {
 
-let firstNum = Number(readSync.question(
-  `- Please enter your first number => \n`));
-while (isNaN(firstNum)) {
-  console.log("The number is not recognized, please try again.");
-  firstNum = Number(readSync.question("- Please enter your first number => \n"));
+  let firstNum = Number(readSync.question(langVersion.askFirstNum));
+
+  while (isNaN(firstNum)) {
+    console.log(langVersion.numberNotRecon);
+    firstNum = Number(readSync.question(langVersion.askFirstNum));
+  }
+
+  let secondNum = Number(readSync.question(langVersion.askSecondNum));
+
+  while (isNaN(secondNum)) {
+    console.log(langVersion.numberNotRecon);
+    secondNum = Number(readSync.question(langVersion.askSecondNum));
+  }
+
+  console.log(langVersion.possibleOperator);
+
+  let operator = readSync.question(langVersion.askOperator).toLowerCase();
+
+  while (!(operator in operations)) {
+    console.log(langVersion.operatorNotRecog);
+    operator = readSync.question(langVersion.askOperator).toLowerCase();
+  }
+  let result = operations[operator](firstNum,secondNum);
+  if (secondNum === 0 && ["/","4","divide","dividir"].includes(operator)) {
+    console.log(langVersion.divisionByZero);
+  } else {
+    console.log(bannerizeInput(`${langVersion.printResult} ${result} `));
+  }
+
+  confirm = readSync.question(langVersion.anotherOperation);
+  while (!acceptableAnswers.includes(confirm)) {
+    console.log(langVersion.wrongAnswer);
+    confirm = readSync.question(langVersion.anotherOperation);
+
+  }
+  console.clear();
+  let quit = ['no','n'];
+  if (quit.includes(confirm.toLowerCase())) {
+    console.log(bannerizeInput(langVersion.farewell));
+    carryOn = false;
+  }
 }
-let secondNum = Number(readSync.question(
-  `- Please enter your second number => \n`));
-while (isNaN(secondNum)) {
-  console.log("The number is not recognized, please try again.");
-  secondNum = Number(readSync.question(
-    `- Please enter your second number => \n`));
-}
-console.log("- To select your operation you can type the following:");
-console.log(`\n
-              + to add\n
-             - to substract\n
-             * to multiply\n
-             / to divide\n`);
-console.log("=> add substract multiply divide \n");
-console.log(`Or also:\n
-             1 for add\n 
-             2 for substract\n
-             3 for multiply\n 
-             4 for divide \n`);
-
-let operator = readSync.question("- Please enter your operator ").toLowerCase();
-
-while (!(operator in operations)) {
-  console.log("That operator is not recognized,please try again.");
-  operator = readSync.question("- Please enter your operator ").toLowerCase();
-}
-
-console.log(bannerizeInput(`The result is ${operations[operator](firstNum,secondNum)} `));
