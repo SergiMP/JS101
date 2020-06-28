6ec6f9cc04dfc86c0707edf1685a3e78f70a8f27
Sergio <smunoz65@gmail.com>
2020-06-28 14:26:25 +0100

    Update calculator.js
    Modified the logic in getting user input using two helper functions.

--- JS101_Small_Programs/calculator.js
+++ JS101_Small_Programs/calculator.js
@@ -28,17 +28,30 @@ let operations = {
   "multiplicar" : multiply, "dividir" : divide,
   "1" : add, "2" : substract, "3" : multiply, "4" : divide
 };
-//Declare while loop conditions.
+//Declare while loop conditions, and helper function isInvalidInput.
 let carryOn = true;
 let confirm;
 const acceptableAnswers = ['yes',"y","yas","no",'n','nope'];
+const quit = ['no','n','nope'];
+
+const isInvalidInput = (input) => {
+  if (typeof input === 'number') {
+    return !isNaN(input);
+  } else if (typeof input === 'string') {
+    return ['es','en'].includes(input) ||
+            acceptableAnswers.includes(input) ||
+            (input in operations);
+  }
+  return false;
+};
 
 //Greet user
 console.log(bannerizeInput(`Greetings, welcome to our calculator`));
-console.log(bannerizeInput(` Please type "es" to switch to the Spanish version of this calculator or "en" for the english version`));
+console.log(bannerizeInput(` Please type "es" to switch to the Spanish version of this calculator or "en" 
+for the english version`));
 let language = readSync.question("Language ===>  ").toLowerCase();
 
-while (!['es','en'].includes(language)) {
+while (!isInvalidInput(language)) {
 
   console.log("- The language is not recognized, please try again.");
   language = readSync.question("Language ===>  ").toLowerCase();
@@ -46,32 +59,54 @@ while (!['es','en'].includes(language)) {
 
 let langVersion = language === "es".trim() ? messages["es"] : messages["en"];
 
+const chooseAgain = (inputType, input) => {
+  input = readSync.question(langVersion[inputType]);
+  return input;
+};
+
+const retrieveChoice = (inputType,input) => {
+  switch (inputType) {
+    case "askFirstNum":
+      console.log(langVersion.numberNotRecon);
+      return Number(chooseAgain('askFirstNum',input));
+    case "askSecondNum":
+      console.log(langVersion.numberNotRecon);
+      return Number(chooseAgain('askSecondNum',input));
+    case "askOperator":
+      console.log(langVersion.operatorNotRecog);
+      return Number(chooseAgain('askOperator',input));
+    case "anotherOperation":
+      console.log(langVersion.wrongAnswer);
+      return chooseAgain('anotherOperation',input);
+    default:
+      return "Not recognized";
+  }
+};
+
 console.log(bannerizeInput(langVersion.greeting.replace(/\s{2}/g,"")));
 //Begin loop, ask numbers, operator, print results.
 while (carryOn !== false) {
 
   let firstNum = Number(readSync.question(langVersion.askFirstNum));
 
-  while (isNaN(firstNum)) {
-    console.log(langVersion.numberNotRecon);
-    firstNum = Number(readSync.question(langVersion.askFirstNum));
+  while (!isInvalidInput(firstNum)) {
+    firstNum = retrieveChoice('askFirstNum',firstNum);
   }
 
   let secondNum = Number(readSync.question(langVersion.askSecondNum));
 
-  while (isNaN(secondNum)) {
-    console.log(langVersion.numberNotRecon);
-    secondNum = Number(readSync.question(langVersion.askSecondNum));
+  while (!isInvalidInput(secondNum)) {
+    secondNum = retrieveChoice('askSecondNum',secondNum);
   }
 
   console.log(langVersion.possibleOperator);
 
   let operator = readSync.question(langVersion.askOperator).toLowerCase();
 
-  while (!(operator in operations)) {
-    console.log(langVersion.operatorNotRecog);
-    operator = readSync.question(langVersion.askOperator).toLowerCase();
+  while (!isInvalidInput(operator)) {
+    operator = retrieveChoice("askOperator",operator);
   }
+
   let result = operations[operator](firstNum,secondNum);
   if (secondNum === 0 && ["/","4","divide","dividir"].includes(operator)) {
     console.log(langVersion.divisionByZero);
@@ -80,13 +115,12 @@ while (carryOn !== false) {
   }
 
   confirm = readSync.question(langVersion.anotherOperation);
-  while (!acceptableAnswers.includes(confirm)) {
-    console.log(langVersion.wrongAnswer);
-    confirm = readSync.question(langVersion.anotherOperation);
-
+  while (!isInvalidInput(confirm)) {
+    confirm = retrieveChoice("anotherOperation",confirm);
   }
+
   console.clear();
-  let quit = ['no','n','nope'];
+
   if (quit.includes(confirm.toLowerCase())) {
     console.log(bannerizeInput(langVersion.farewell));
     carryOn = false;
