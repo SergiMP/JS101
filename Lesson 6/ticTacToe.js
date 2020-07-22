4d6d10f0c1579fa4798f2b014ec27312f2dfceac
SergiMP <smunoz02@dcs.bbk.ac.uk>
2020-07-22 16:15:39 +0100

    Update ticTacToe.js

--- Lesson 6/ticTacToe.js
+++ Lesson 6/ticTacToe.js
@@ -1,6 +1,9 @@
 "use strict";
 let readsync = require("readline-sync");
 
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 let p = { 1: "1", 2: "2", 3:"3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9: "9"}
 
 let initializeBoard = () => {
@@ -9,6 +12,8 @@ let initializeBoard = () => {
     keys.forEach(key => result[key]= "");
     return board;
 }
+
+const symbols = ["X","O"];
 
 let threeBythreeBoard = () => `
                               |   | 
@@ -22,14 +27,25 @@ let threeBythreeBoard = () => `
                               |   | 
                            `;
 
+let displayBoard = (board) => {
+  console.clear();
+  console.log(board)
+}
+
 let availableMoves = Object.keys(p);
 
 let isValidMove = (move) => availableMoves.includes(move);
 
-let userSymbol = readsync.question(`=> Please make your choice... X or O?`).toLocaleLowerCase();
+let userSymbol = readsync.question(`=> Please make your choice... X or O ? `);
 
-while ( userSymbol !== "X")
+while ( !symbols.includes(userSymbol) ){
+  console.log("Not a valid symbol...at least yet...");
+  userSymbol = readsync.question(`=> Please make your choice... X or O ? `).trim().toUpperCase();                                          
+};
 
+let computerSymbol = symbols.filter(x => x !== userSymbol);
+
+while (availableMoves.length !== 0) {
 let displayMoves = () => `=> Please select one of the available moves: ${availableMoves.join(",")} `;
 
 let playerMove = readsync.question(`${displayMoves()}`);
@@ -41,5 +57,21 @@ while (!(isValidMove(playerMove))) {
 
 p[playerMove] = userSymbol;
 
-console.log(threeBythreeBoard());
-//console.log(displayMoves());                            
+availableMoves = availableMoves.filter(x => x !== playerMove);
+
+let computerChoice = "";
+
+while (computerChoice === ""){
+  console.log(`Available moves for computer are ${availableMoves}`)
+   computerChoice =availableMoves[Math.floor(Math.random() * availableMoves.length)];
+   console.log(`Computer chose ${computerChoice}`);
+}
+
+p[computerChoice] = computerSymbol;
+
+availableMoves = availableMoves.filter(x => x !== computerChoice);
+
+
+console.log(displayBoard(threeBythreeBoard()));
+}
+                          
